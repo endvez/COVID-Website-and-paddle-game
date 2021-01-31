6c31cb4428d80705ec7a1247da08a2f9b46becea
endvez <77261912+endvez@users.noreply.github.com>
2021-01-31 10:24:53 -0800

    Create flask.py

---
+++ flask.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask import render_template
+import requests
+from bs4 import BeautifulSoup
+app = Flask(__name__)
+
+@app.route('/')
+
+def hello_world():
+
+  #return 'Hello, world!'
+
+  return render_template("hello.html")
+@app.route('/result') #scrapper
+
+def get_result():
+
+  source = requests.get("https://www.worldometers.info/coronavirus/").text
+
+  return source
+@app.route('/game')
+
+def paddle():
+
+  return render_template("game.html")
+@app.route('/image')
+
+def get_image():
+
+  source = requests.get("https://specials-images.forbesimg.com/imageserve/5e6b201b7d6f2600068f59ea/960x0.jpg?fit=scale/").text
+
+  return source
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8089)
